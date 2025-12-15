c57e1ee113e439524b3f8fe96b63d15168c63e69
Linosa404 <s0592919@htw-berlin.de>
2025-12-15 17:19:11 +0100

    delete deployment

--- deployment-php81/config/tracking.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'online_threshold_minutes' => env('EMPLOYEE_ONLINE_THRESHOLD_MINUTES', 10),
-    'map_auto_refresh_seconds' => env('MAP_AUTO_REFRESH_SECONDS', 30),
-    'rate_limit_per_minute' => env('RATE_LIMIT_PER_MINUTE', 60),
-];
